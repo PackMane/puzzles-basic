085054bbbc697da4dc7a20f07cfb0b26fd5d931e
Nicholasp1992 <nicholasowenpacker@gmail.com>
2021-04-12 17:48:12 -0600

    challenge-complete

---
+++ challenge-13/array.js
@@ -0,0 +1,23 @@
+// make an array with 13 items
+// get items 4-9 in the array and store them in a value
+// print the items
+
+const arr = [
+  'poo',
+  'ass',
+  'big',
+  'donkey',
+  'muppet',
+  'prince',
+  'toffee',
+  'coffee',
+  'toilet',
+  'fridge',
+  'conditioner',
+  'car',
+  'bike',
+];
+
+let fourOfNine = arr.slice(3, 9);
+
+console.log(fourOfNine);
