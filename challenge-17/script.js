583808dabbd90312a160996546fde5b1d107d73d
Nicholasp1992 <nicholasowenpacker@gmail.com>
2021-04-12 20:25:45 -0600

    challenge-17 struggle

---
+++ challenge-17/script.js
@@ -0,0 +1,12 @@
+// make an array of incrementing numbers form 0 to 10
+// write a function that takes an array and a number N
+// return a new array with all the numbers in the array incremented by N
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function arrAdd(newArr, n) {
+  arr.add[n];
+  return newArr;
+}
+
+console.log(arrAdd(arr, 2));
